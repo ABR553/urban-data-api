6fb38ba37402124552bd259b1091fb0289d2d9dc
abr553 <ariel960822@gmail.com>
2023-03-23 22:23:34 +0100

    load data from csv

--- house_info/models.py
+++ house_info/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import csv
+import re
 
 # Create your models here.
 class HouseInfo(models.Model):
@@ -13,10 +15,40 @@ class HouseInfo(models.Model):
     end_week = models.IntegerField()
     typology_type = models.PositiveSmallIntegerField(null=True, blank=True, choices=[(0, 'Tipologia 0'), (1, 'Tipologia 1'), (2, 'Tipologia 2'), (3, 'Tipologia 3'), (4, 'Tipologia 4'), (5, 'Tipologia 5'), (6, 'Tipologia 6')], default=None)
     id = models.IntegerField(unique=True, primary_key=True)
-    lat = models.DecimalField(max_digits=50, decimal_places=50)
-    lng = models.DecimalField(max_digits=50, decimal_places=50)
+    coordinates = models.CharField(max_length=256, null=True, blank=True)
     bondary_id = models.IntegerField()
     id_uda = models.CharField(max_length=32)
     title = models.CharField(max_length=128)
     listing_type = models.PositiveSmallIntegerField(null=True, blank=True, choices=[(0, 'Tipo 0'), (1, 'Tipo 1')], default=None)
     date = models.DateTimeField()
+
+    @classmethod
+    def seed_data(self):
+        with open('/code/assets.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in list(reader):
+                if row['build_status']:
+                    try:
+                        construction_type = int(row['construction_type'])
+                    except:
+                        construction_type = None
+
+                    HouseInfo.objects.create(
+                        build_status=int(row['build_status']),
+                        is_active=row['is_active'] == "True",
+                        start_month=int(row['start_month']),
+                        construction_type=construction_type,
+                        date_diff=int(row['date_diff']),
+                        description=row['description'],
+                        date_in=row['date_in'],
+                        property_type=int(row['property_type']),
+                        end_week=int(row['end_week']),
+                        typology_type=int(row['typology_type']),
+                        id=int(re.sub(r"\D", "", row['id'])),
+                        coordinates=row['coordinates'],
+                        bondary_id=int(row['boundary_id']),
+                        id_uda=row['id_uda'],
+                        title=row['title'],
+                        listing_type=int(row['listing_type']),
+                        date=row['date']
+                    )
